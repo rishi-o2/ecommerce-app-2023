1d6ae75e6ad7252ac968dd5c31d8a9a08c775f68
Rishi Raj <116630999+rishi-o2@users.noreply.github.com>
2024-01-20 21:54:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
 import express from "express";
-import colors from "colors";
 import dotenv from "dotenv";
-import morgan from "morgan";
+import mongoose from "mongoose";
+import morgan from 'morgan';
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
+import authroutes from "./routes/authroutes.js";
+import CategoryRoutes from "./routes/CategoryRoutes.js";
 import cors from "cors";
+import ProductRoute from "./routes/ProductRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-//configure env
-dotenv.config();
-
-//databse config
-connectDB();
-
-//rest object
 const app = express();
 
-//middelwares
-app.use(cors());
+// Workaround for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+connectDB();
+
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(cors());
+app.use("/api/v1/auth", authroutes);
+app.use("/api/v1/category", CategoryRoutes);
+app.use("/api/v1/product", ProductRoute);
+app.use(express.static(path.join(__dirname, './client/build')));
 
-//routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+dotenv.config();
 
-//rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-//PORT
-const PORT = process.env.PORT || 8080;
-
-//run listen
+const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
+  console.log(`server running on port ${PORT}`);
 });
+
